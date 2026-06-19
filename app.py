af99043c80b0b3977675fb56147df5a3460acb9b
Portela <pauloestevan2803@gmail.com>
2026-06-19 10:02:19 -0300

    Ajuste da conexao do banco para producao

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import psycopg2
 
 app = Flask(__name__)
 
+import os
+import psycopg2
+
 conn = psycopg2.connect(
-    host="localhost",
-    database="controle_abonadas",
-    user="recepcao",
-    password="1234"
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD")
 )
 
 cursor = conn.cursor()
